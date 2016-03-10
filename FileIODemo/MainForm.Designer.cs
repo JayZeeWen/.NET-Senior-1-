9f5b1c93cae8759d213d543d84ae1aa875b673fc
JayZeeWen <562230790@qq.com>
2016-03-10 23:11:41 +0800

    3.10

---
+++ FileIODemo/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FileIODemo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.txtFileContent = new System.Windows.Forms.RichTextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(456, 10);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "OpenFile";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(438, 21);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // txtFileContent
+            // 
+            this.txtFileContent.Location = new System.Drawing.Point(12, 39);
+            this.txtFileContent.Name = "txtFileContent";
+            this.txtFileContent.Size = new System.Drawing.Size(637, 167);
+            this.txtFileContent.TabIndex = 2;
+            this.txtFileContent.Text = "";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(537, 10);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "SaveFile";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 337);
+            this.Controls.Add(this.txtFileContent);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpenFile);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.RichTextBox txtFileContent;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ FileIODemo/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileIODemo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog ())
+            {
+                if(ofd.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                {
+                    return;
+                }
+
+                //读取文件
+                using(FileStream fs = new FileStream (ofd.FileName,FileMode.Open,FileAccess.Read))
+                {
+                    using(StreamReader sr = new StreamReader (fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            this.txtFileContent.Text += line;
+                        }   
+                    }
+                }
+            }
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            using(SaveFileDialog sfd = new SaveFileDialog ())
+            {
+                if(sfd.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+                //File.WriteAllText(sfd.FileName, txtFileContent.Text, Encoding.Default);
+                //using(StreamWriter sw = new StreamWriter (sfd.FileName,false,Encoding.Default,1024*1024))
+                //{
+                //    sw.Write(txtFileContent.Text);
+                //    sw.Flush();
+                //}
+
+                using (FileStream fs = new FileStream (sfd.FileName,FileMode.OpenOrCreate,FileAccess.ReadWrite))
+                {
+                    string str = txtFileContent.Text;
+                    byte[] data = Encoding.Default.GetBytes(str);
+                    fs.Write(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
