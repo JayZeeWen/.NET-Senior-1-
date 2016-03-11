02457a1479382c3f373d2f2cc47c0fbcdef831db
JayZeeWen <562230790@qq.com>
2016-03-11 23:24:28 +0800

    Signed-off-by: JayZeeWen <562230790@qq.com>

---
+++ WinFormThread/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoop_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(() => {
+                while (true)
+                {
+                    if(btnLoop.InvokeRequired)//判断是否是其他线程创建的控件 
+                    {
+                        //寻找创建该控件的线程，执行委托实例
+                        btnLoop.Invoke(new Action<string>(s => { this.btnLoop.Text = s; }), DateTime.Now.ToString());
+                    }
+                    else
+                    {
+                        this.btnLoop.Text = DateTime.Now.ToString();
+                    }
+                    Console.WriteLine(DateTime.Now.ToString());
+                }
+            });
+
+            thread.IsBackground = true;
+            thread.Start();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WinFormThread/LuckDraw.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormThread
+{
+    public partial class LuckDraw : Form
+    {
+        private List<Label> lbList = new List<Label>();
+
+        private Thread threadStard;
+        private bool isRunning = false;
+
+        public LuckDraw()
+        {
+            InitializeComponent();
+        }
+
+        private void LuckDraw_Load(object sender, EventArgs e)
+        {
+            for(int i = 0 ;i< 6;i++)
+            {
+                Label lb = new Label();
+                lb.Text = "0";
+                lb.AutoSize = true;
+                lb.Location = new Point((i + 1) * 50, 100);
+                this.Controls.Add(lb);
+                lbList.Add(lb);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+                Random r = new Random ();
+                isRunning = true;
+                //Change the number of label 
+                while(isRunning)
+                {
+                    foreach(Label item in lbList)
+                    {
+                        string str = r.Next(0, 10).ToString();
+                        if(item.InvokeRequired)
+                        {
+                            item.Invoke(new Action<string>(s => {
+                                item.Text = str;
+
+                            }),str);
+                        }
+                        else
+                        {
+                            item.Text = str;
+                        }
+                    }
+                    Thread.Sleep(200);
+                }
+            }));
+
+            thread.IsBackground = true;
+            thread.Start();
+            threadStard = thread;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            isRunning = false;
+            //if(threadStard == null || !threadStard.IsAlive)
+            //{
+            //    return;
+            //}
+            //threadStard.Abort();
+
+        }
+    }
+}
