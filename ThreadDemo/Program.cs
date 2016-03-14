3875d4ab222ade1efb689c4b9190a01f18d96ac3
JayZeeWen <562230790@qq.com>
2016-03-14 23:14:17 +0800

    Signed-off-by: JayZeeWen <562230790@qq.com>

--- ThreadDemo/Program.cs
+++ ThreadDemo/Program.cs
@@ -12,25 +12,82 @@ namespace ThreadDemo
         //Main:程序入口，clr一开始会创建一个默认的主线程（前台），指向Main方法。
         static void Main(string[] args)
         {
-            Thread thread = new Thread(delegate() { 
-                while(true)
-                {
-                    Console.WriteLine(DateTime.Now);
-                    Thread.Sleep(500);
-                }
-            });
-            //默认是前台线程   当所有的前台线程都结束之后进程才回退出；后台线程不会阻塞进程的退出。
-            thread.IsBackground = true;
-            thread.Start();
+            #region 线程
+            //Thread thread = new Thread(delegate() { 
+            //    while(true)
+            //    {
+            //        Console.WriteLine(DateTime.Now);
+            //        Thread.Sleep(500);
+            //    }
+            //});
+            ////默认是前台线程   当所有的前台线程都结束之后进程才回退出；后台线程不会阻塞进程的退出。
+            //thread.IsBackground = true;
+            //thread.Start();
 
-            //主线程可以做其他的事情
-            while(true)
+            ////主线程可以做其他的事情
+            //while(true)
+            //{
+            //    Console.WriteLine("I am in Main Thread");
+            //    Thread.Sleep(500);
+            //}
+
+            #endregion
+
+            #region 生产者消费者问题
+
+            //create a pool 
+            MyConncetoin[] connectionArray = new MyConncetoin[100];
+            //index
+            int index = 0;
+            //create 10 customer
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("I am in Main Thread");
-                Thread.Sleep(500);
+                // one customer
+                Thread thread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        if (index >= 0)
+                        {
+                            connectionArray[index - 1] = null;
+                            Console.WriteLine("create Customer");
+                            index--;
+                        }
+                        Thread.Sleep(200);
+                    }
+                });
+                thread.IsBackground = true;
+                thread.Start();
             }
 
+            //create 5 producer
+            for (int i = 0; i < 5; i++)
+            {
+                Thread thread = new Thread(() =>
+                {
+                    while (true)
+                    {
+                        if (index < 99)
+                        {
+                            connectionArray[index] = new MyConncetoin();
+                            Console.WriteLine("create Producer");
+                            index++;
+                        }
+                        Thread.Sleep(500);
+                    }
+                });
+                thread.IsBackground = true;
+                thread.Start();
+            }
+
+            #endregion
             Console.ReadKey();
         }
+
+        public class MyConncetoin
+        {
+
+        }
+
     }
 }
---
+++ ThreadPoolDemo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //线程池的线程都是后台线程，优势：线程可以进行重用，任务执行完成后并不会马上释放对象
+            //启动一个线程：开辟一个内存空间，1M内存。
+            //在线程数量非常多的时候，操作系统在切换线程上会耗费较多的计算。
+            //同时，线程的创建同样消耗资源
+            //ThreadPool.QueueUserWorkItem(s => 
+            //{
+            //    Console.WriteLine(s);
+            //},"ssssssssss");
+            #region 线程池优势演示
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            //for (var i = 0; i < 100; i++ )
+            //{
+            //    new Thread(() => 
+            //    {
+            //        int i1 = 0;
+            //        //Console.WriteLine(++i1);
+            //        i1++;
+            //    }).Start();
+
+            //}
+            //sw.Stop();
+            //Console.WriteLine(sw.Elapsed.TotalSeconds);
+            //sw.Restart();
+            //for (var i = 0; i < 100; i++ )
+            //{
+            //    ThreadPool.QueueUserWorkItem(s =>
+            //    {
+            //        int i1= 0 ;
+            //        Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //        //Console.WriteLine(++i1);
+            //        i1++;
+            //    });
+            //}
+            //sw.Stop();
+            //Console.WriteLine(sw.Elapsed.TotalSeconds);
+            #endregion 
+
+            #region 异步委托
+
+            Console.WriteLine("Main Thread:" + Thread.CurrentThread.ManagedThreadId);
+
+            Func<int, int, string> delFunc = (a, b) => 
+            {
+                Console.WriteLine("Delegate Thread :" + Thread.CurrentThread.ManagedThreadId);
+                return (a + b).ToString(); 
+            };
+            //string str = delFunc(3, 4);
+
+            //异步调用委托
+            //delFunc.BeginInvoke(3, 4, null, null);
+            //Console.WriteLine(str);
+            //得到异步委托的返回值
+            //EndInvoke会阻塞当前执行线程
+            IAsyncResult result = delFunc.BeginInvoke(3, 4, null, null);
+            string str = delFunc.EndInvoke(result);
+            Console.WriteLine(str);
+
+            #endregion 
+            Console.ReadKey();
+        }
+    }
+}
